a8bd9f203d0433d3c1d716ab48acdff249fe4376
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-16 19:37:09 +0200

    starting tour app

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,34 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+  return (
+    <main>
+      <section>
+
+        <div className="title">
+          <h2>tours</h2>
+          <div className="underline"></div>
+        </div>
+
+        <div>
+          <article className="single-tour">
+            <img src="" alt="" />
+            <footer>
+              <div className="tour-info">
+                <p>
+                  details 
+                  <button>read more</button>
+                </p>
+                <button class="delete-btn"></button>
+              </div>
+            </footer>
+          </article>
+        </div>
+
+      </section>
+    </main>
+  )
 }
 
 export default App
