b3790510d7e99dd9e1b9d6f0a1d86d1436e45000
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-17 09:52:45 +0200

    set up html and added items data to menu component

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,29 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <div className="btn-container">
+          <button className="filter-btn">All</button>
+          <button className="filter-btn">breaksfast</button>
+          <button className="filter-btn">lunch</button>
+          <button className="filter-btn">shakes</button>
+        </div>
+
+        <div className="section-center">
+          {items.map((item)=>{
+            return <Menu key={item.id} {...item} />
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App;
