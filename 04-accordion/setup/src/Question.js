2d544673a1dcf408dca54203a58246cb03d70156
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-17 09:27:11 +0200

    created function to hide and unhide info

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -2,13 +2,22 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Question = ({title, info}) => {
+  const [hidden, setHidden] = useState('none');
+  
+  const handleHiddenEle = () => {
+    if (hidden === 'none') {
+      setHidden("block");
+    } else {
+      setHidden("none");
+    }
+  }
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn"></button>
+        <button className="btn" onClick={handleHiddenEle}>{AiOutlinePlus}</button>
       </header>
-      <p>{info}</p>
+      <p style={{display: hidden}}>{info}</p>
     </article>
   )
 };
