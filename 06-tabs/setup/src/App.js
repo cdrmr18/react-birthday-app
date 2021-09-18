05ba9dc00559f60e00ab30a9dfbb036dbde5306a
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-18 17:32:02 +0200

    added experience and loading state

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,6 +4,28 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [experience, setExperience] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getExperienceData = async () => {
+    const response = await fetch(url);
+    const experienceData = response.json()
+    setExperience(experienceData);
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    getExperienceData();
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Page is loading</h1>
+      </section>
+    )
+  }
+
   return (
     <section className="section">
       <div className="title">
@@ -20,7 +42,7 @@ function App() {
           <h4>cooker</h4>
           <p className="job-date">Nov 2020</p>
           <div className="job-desc">
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="job-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"></path></svg>
+            <FaAngleDoubleRight className="job-icon" />
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, minus!</p>
           </div>
         </article>
