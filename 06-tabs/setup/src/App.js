cb1b944949f5ef7d9a6dc9117ddb90dae5955a8d
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-18 17:19:33 +0200

    set up html elements

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,30 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          <button className="job-btn false">TOMMY</button>
+          <button className="job-btn active-btn">TOMMY</button>
+        </div>
+        <article className="job-info">
+          <h3>Engineering Intern</h3>
+          <h4>cooker</h4>
+          <p className="job-date">Nov 2020</p>
+          <div className="job-desc">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="job-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"></path></svg>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, minus!</p>
+          </div>
+        </article>
+      </div>
+      <button type="button" className="btn">more info</button>
+    </section>
+  )
 }
 
 export default App
