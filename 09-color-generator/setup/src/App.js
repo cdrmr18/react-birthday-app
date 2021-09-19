834cda90bdf37d8e7de8993ae5a76e45054c9dfb
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-19 13:04:05 +0200

    set up html and handling submit functions

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,42 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10)
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  }
+  return (
+   <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          value={color} 
+          placeholder="#f15025" 
+          onChange={(e)=>setColor(e.target.value)}
+          className={`${error? 'error' : null}`} />
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <article className="color false" style={{backgroundColor:"rgb(255, 255, 255)"}}>
+          <p className="percent-value">List</p>
+          <p className="color-value"></p>
+        </article>
+      </section>
+   </>
+  )
 }
 
 export default App
