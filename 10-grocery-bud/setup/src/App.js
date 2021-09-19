ff5b56c046801a4eb1c2f92d6d0df27dfe3314ba
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-19 18:00:41 +0200

    set up html stucture

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -4,7 +4,7 @@ import Alert from './Alert'
 
 function App() {
   const [name, setName] = useState('');
-  const [alert, setAlert] = useState({});
+  const [alert, setAlert] = useState({show:false, msg:'',type:''});
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
@@ -12,30 +12,37 @@ function App() {
 
   const handleSubmit = (e) => {
     e.prventDefault();
-    const newItem = value;
-    setList([...list, newItem]);
-    setValue('');
+    // const newItem = value;
+    // setList([...list, newItem]);
+    // setValue('');
   }
   useEffect(()=>{
     console.log('effect');
   },[])
+
   return (
     <section className="section-center">
-      <form action="
-      " className="grocery-form" onSubmit={handleSubmit}>
+      <form
+        className="grocery-form" 
+        onSubmit={handleSubmit}
+        >
+          {alert.show && <Alert />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input 
           type="text" 
           className="grocery" 
           placeholder="e.g. eggs"
-          value={value}
-          onChange={(e)=>setValue(e.target.value)}
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
           />
-          <button className="submit-btn">submit</button>
+          <button className="submit-btn">{isEditing? 'edit' : 'submit'}</button>
         </div>
       </form>
-      {list.length > 0 && <List list={list} />}
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear List</button>
+      </div>
     </section>
   )
 }
