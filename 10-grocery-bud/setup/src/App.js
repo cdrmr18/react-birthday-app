bcacc71e9326928ec9e42c78d1810a7a12335126
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-19 17:22:24 +0200

    added event to update input value captureed

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,26 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [value, setValue] = useState('');
+  console.log(value);
+  return (
+    <section className="section-center">
+      <form action="
+      " className="grocery-form">
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input 
+          type="text" 
+          className="grocery" 
+          placeholder="e.g. eggs"
+          value={value}
+          onChange={(e)=>setValue(e.target.value)}
+          />
+          <button className="submit-btn">submit</button>
+        </div>
+      </form>
+    </section>
+  )
 }
 
 export default App
