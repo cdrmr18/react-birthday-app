30ba98f0c571a61d63c21e36d48999ee7dc2a81b
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-19 11:30:11 +0200

    added input onchange to get value and update count

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
- const [text, setText] = useState(data); 
-  
+  const [text, setText] = useState(data); 
+  const [count, setCount] = useState(0);
+
+  const handleCount = (e) => {
+    setCount(e.target.value);
+  }
+
   return (
     <section className="section-center">
       <h3>TIRED OF BORING LOREM IPSUM?</h3>
       <form action="" className="lorem-form">
         <label htmlFor="amount">paragraphs:</label>
-        <input type="number" />
+        <input type="number" value={count} name="amount" id="amount" onChange={handleCount}/>
         <button className="btn">generate</button>
       </form>
       <article className="lorem-text">
